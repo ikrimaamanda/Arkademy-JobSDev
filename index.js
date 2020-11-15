845d08ae9076d41a9fb1b8765e005f2426f22b09
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2020-11-15 20:11:39 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express('express')
 // const db = require('./src/helpers/db')
+
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const accountRouter = require('./src/routers/accounts')
